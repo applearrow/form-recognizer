91dc383f975376e5dec7aa2e4057c84fd9f85994
Pablo Manzano <manzanoflecha@gmail.com>
2022-11-22 16:13:38 +0100

    compare_results uses only the images received

--- recognizer_utils/compare_results.py
+++ recognizer_utils/compare_results.py
@@ -2,7 +2,7 @@ import json
 
 from termcolor import colored
 
-def compare_results (correct_values_json, results_folder = 'results'):
+def compare_results (images, correct_values_json, results_folder = 'results'):
   
   matches = {}
   fiels_to_compare = [ 'MerchantName', 'Total', 'TransactionDate' ]
@@ -11,22 +11,23 @@ def compare_results (correct_values_json, results_folder = 'results'):
 
   with open(correct_values_json) as json_file:
     correct_values = json.load(json_file)
-    num_correct_values = len(correct_values)
+    num_correct_values = len(images)
  
     for result in correct_values:
       imageFile = result['ImageFile']
-      with open(f"{results_folder}/{imageFile}.json", 'r') as extracted_values_json:
-        extracted_values = json.load(extracted_values_json)
-        for field in fiels_to_compare:
-          try: 
-            correct = result[field]
-            extracted = extracted_values[field]
-            if extracted == correct:
-              matches[field] += 1
-            else:
-              print (colored(f'{imageFile}:{field}: "{extracted}" <> "{correct}"', 'red'))  
-          except:
-            pass
+      if imageFile in images:
+        with open(f"{results_folder}/{imageFile}.json", 'r') as extracted_values_json:
+          extracted_values = json.load(extracted_values_json)
+          for field in fiels_to_compare:
+            try: 
+              correct = result[field]
+              extracted = extracted_values[field]
+              if extracted == correct:
+                matches[field] += 1
+              else:
+                print (colored(f'{imageFile}:{field}: "{extracted}" <> "{correct}"', 'red'))  
+            except:
+              pass
 
   print (matches)
   print ([matches[x]/num_correct_values for x in matches], num_correct_values)
